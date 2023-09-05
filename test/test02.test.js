7d6dfc46ebcb1b3b73aea1b8c55b935478a4c7db
Takeyuki UEDA <ueda@latelierdueda.com>
2023-09-05 21:20:05 +0900

    updated

--- test/test02.test.js
+++ test/test02.test.js
@@ -16,11 +16,53 @@ describe('getListItems test', function () {
 //      console.log("file", file)
   });
 
-  it('only attr', function(){
+  it('a', function(){
     console.log("items[0]", items[0])
     assert.equal(items[0].attr, 'a')
     assert.equal('path' in items[0], false)
     assert.equal('title' in items[0], false)
+  })
+
+  it('http://uedasoft.com', function(){
+    console.log("items[1]", items[1])
+    assert.equal('attr' in items[1], false)
+    assert.equal(items[1].path, 'http://uedasoft.com')
+    assert.equal('title' in items[1], false)
+  })
+
+  it('https://uedasoft.com', function(){
+    console.log("items[2]", items[2])
+    assert.equal('attr' in items[2], false)
+    assert.equal(items[2].path, 'https://uedasoft.com')
+    assert.equal('title' in items[2], false)
+  })
+  
+  it('file://uedasoft', function(){
+    console.log("items[3]", items[3])
+    assert.equal('attr' in items[3], false)
+    assert.equal(items[3].path, 'file://uedasoft')
+    assert.equal('title' in items[3], false)
+  })
+
+  it('/abc', function(){
+    console.log("items[4]", items[4])
+    assert.equal('attr' in items[4], false)
+    assert.equal(items[4].path, '/abc')
+    assert.equal('title' in items[4], false)
+  })
+
+  it('./abc', function(){
+    console.log("items[5]", items[5])
+    assert.equal('attr' in items[5], false)
+    assert.equal(items[5].path, './abc')
+    assert.equal('title' in items[5], false)
+  })
+
+  it('abc/deb', function(){
+    console.log("items[6]", items[6])
+    assert.equal('attr' in items[6], false)
+    assert.equal(items[6].path, 'abc/deb')
+    assert.equal('title' in items[6], false)
   })
 
 });
