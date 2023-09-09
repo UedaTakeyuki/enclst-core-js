7017d593b977b4b103fc138fad5d416607e3b3a4
Takeyuki UEDA <ueda@latelierdueda.com>
2023-09-09 18:07:51 +0900

    add class EncLstCore

---
+++ src/enclstcore.js
@@ -0,0 +1,13 @@
+/**
+ * クラスXの説明
+ */
+export class EncLstCore {
+  /**
+   * クラスXのコンストラクタの説明
+   * @param {number} p1 パラメータ１
+   * @param {number} p2 パラメータ２
+   */
+  constructor(str){}
+  getTitle(){}
+  getListItems(){}
+}
---
+++ test/test04.test.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+import {EncLstCore} from '../src/enclstcore.js'
+
+test("default", function(){
+  console.log("EncLstCore", EncLstCore)
+  expect(true).toBe(true)
+})
